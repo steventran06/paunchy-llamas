2c4d9342d4d420bffd93e1bb9b7ae5b06a0d48a2
byeo630 <byeo630@gmail.com>
2016-04-20 11:17:24 -0700

    (feat) Add new tab button and styling

--- client/app/codeshare/codeshare.js
+++ client/app/codeshare/codeshare.js
@@ -3,14 +3,14 @@ angular.module('codellama.codeshare', [])
     this.code = '';
   })
   .controller('CodeshareController', function($scope) {
-    $scope.code = 'console.log(yaya)';
-
-    var myCodeMirror = CodeMirror(document.getElementById('codeshareBox'), {
-      value: '\'Write your code here\'',
-      mode:  'javascript',
-      theme: 'ambiance',
-      lineNumbers: true
-    });
-    // for some reason CodeMirror(...) creates two codemirror dom elements, and so one is removed as a naive solution
-    document.getElementByClass('Codemirror')[1].remove();
+    $scope.addCodeshare = function () {
+      var myCodeMirror = CodeMirror(document.getElementById('codeshare-input'), {
+        value: '\'Write your code here\'',
+        mode:  'javascript',
+        theme: 'ambiance',
+        lineNumbers: true
+      });
+      // for some reason CodeMirror(...) creates two codemirror dom elements, and so one is removed as a naive solution
+      // document.getElementByClass('Codemirror')[1].remove();
+    };
   });
