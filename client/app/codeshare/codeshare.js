7a74495fa8e0e27c08fade4ff6ea27f86a2d7497
byeo630 <byeo630@gmail.com>
2016-04-20 10:42:49 -0700

    (feat) Add line numbers in gutter and make text align left

--- client/app/codeshare/codeshare.js
+++ client/app/codeshare/codeshare.js
@@ -6,9 +6,11 @@ angular.module('codellama.codeshare', [])
     $scope.code = 'console.log(yaya)';
 
     var myCodeMirror = CodeMirror(document.getElementById('codeshareBox'), {
-      value: 'Write your code here',
+      value: '\'Write your code here\'',
       mode:  'javascript',
-      theme: 'ambiance'
+      theme: 'ambiance',
+      lineNumbers: true
     });
+    // for some reason CodeMirror(...) creates two codemirror dom elements, and so one is removed as a naive solution
     document.getElementByClass('Codemirror')[1].remove();
   });
