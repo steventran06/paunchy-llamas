48ff1cd4275fbbc4e6378413bac9ab90f00e085c
byeo630 <byeo630@gmail.com>
2016-04-19 19:43:44 -0700

    (feat) add codeshare html and js files

---
+++ client/app/codeshare/codeshare.js
@@ -0,0 +1,7 @@
+angular.module('codellama.codeshare', [])
+  .service('CodeshareService' function() {
+    this.code = '';
+  })
+  .controller('CodeshareController', function($scope) {
+    
+  });
