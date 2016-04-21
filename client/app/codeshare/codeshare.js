a0612c9470f9fc4a64b0dba81a2cddec3a60bc19
byeo630 <byeo630@gmail.com>
2016-04-20 21:06:25 -0700

    (feat) Create working version of codeshare

--- client/app/codeshare/codeshare.js
+++ client/app/codeshare/codeshare.js
@@ -12,9 +12,49 @@ angular.module('codellama.codeshare', [])
     socket.on('connect', function() {
       socket.emit('join codeshare room', path);
     });
-    socket.on('code', function(code) {
-      console.log(code);
-    });
+
+    // Debounce function
+    function debounce(func, wait, immediate) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    };
+
+    // Emit code
+    var emit = debounce(function(target, name, args) {
+      var code = this.getValue();
+      var text = JSON.stringify(code);
+      socket.emit('type code', text, path);
+    }, 1000);
+
+    // Load in the Google Difference, Match, Patch algorithm
+    var dmp = new diff_match_patch();
+    var patch_text = '';
+
+    // This finds the difference between the code of user1 and user2
+    $scope.codeDiff = function(code1, code2) {
+      var d = dmp.diff_main(code1, code2, true);
+      var prettyD = dmp.diff_prettyHtml(d);
+      var patch_list = dmp.patch_make(code1, code2, d);
+      patch_text = dmp.patch_toText(patch_list);
+      return $scope.codePatch(code1);
+    };
+
+    $scope.codePatch = function(code) {
+      var patches = dmp.patch_fromText(patch_text);
+      var results = dmp.patch_apply(patches, code);
+      return results[0];
+    };
+
     $scope.addCodeshare = function () {
       var myCodeMirror = CodeMirror(document.getElementById('codeshare-input'), {
         value: '\'Write your code here\'',
@@ -22,10 +62,14 @@ angular.module('codellama.codeshare', [])
         theme: 'ambiance',
         lineNumbers: true
       });
-      myCodeMirror.on('change', function(target, name, args) {
-        var code = myCodeMirror.getValue();
-        var text = JSON.stringify(code);
-        socket.emit('type code', text, path);
-      })
+      myCodeMirror.on('change', emit.bind(myCodeMirror));
+      socket.on('code', function(theirCode) {
+        var myCode = myCodeMirror.getValue();
+        var diff = JSON.parse($scope.codeDiff(myCode, theirCode));
+        if (diff !== myCode) {
+          myCodeMirror.setValue(diff);
+        }
+        // myCodeMirror.setValue(code);
+      });
     };
   });
