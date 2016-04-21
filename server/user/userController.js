f2d5af2016df4b1303f770de2d7eab15c4cf7d10
Krista Moroder <kmoroder@gmail.com>
2016-04-21 12:30:41 -0700

    (Feat) Add fields for new user to delcare available times

--- server/user/userController.js
+++ server/user/userController.js
@@ -91,6 +91,7 @@ module.exports = {
   saveProfile: function(req, res) {
     // helpers.decode gives us the username from the token on this request
     var update = function(req, res) {
+      if (req.body.times) {req.body.times = req.body.times.join(',').split(/\W+/); }
       if (req.body.subjects) { req.body.subjects = req.body.subjects.join(',').toLowerCase().split(/\W+/); }
       if (req.body.location.city) { req.body.location.city = req.body.location.city.toLowerCase(); }
       updateUser({username: req.user.username}, req.body, {new: true}, function(err, doc) {
