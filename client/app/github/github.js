2c984d3ece0ca2540283f3626032a1da79284a0a
Tor Sinclair <tor@romanesco.se>
2016-04-21 18:54:50 -0700

    (fix) Add comments

--- client/app/github/github.js
+++ client/app/github/github.js
@@ -2,15 +2,18 @@ angular.module('codellama.github', [])
 
 .controller('gitHubDataController', ['$scope', '$http', 'SearchService', '$routeParams', function($scope, $http, SearchService, $routeParams){
 
+// inject service into controller
+// inject routeParams to get github profile
+
   var githubHandle = $routeParams.github;
 
+  // send get request to github, receive max 30 repositories
   $scope.username = githubHandle;
   $http.get("https://api.github.com/users/" + $scope.username)
     .success(function (data) {
       $scope.userData = data;
       loadRepos();
       });    
-
   var loadRepos = function () {
       $http.get($scope.userData.repos_url)
           .success(function (data) {
@@ -18,28 +21,6 @@ angular.module('codellama.github', [])
           });
   };
 
+  // sort repositories by updated_at
   $scope.predicate = '-updated_at';
-
-  // $scope.onSubmit = function(){
-  
-  // if($scope.title === undefined) { return; }
-  //   $scope.reposLoaded = false;
-  //   $scope.userLoaded = false;
-  //   $scope.username = 'torsinclair';
-  //     $http.get("https://api.github.com/users/" + $scope.username)
-  //       .success(function (data) {
-  //         console.log($scope.username);
-  //         $scope.userData = data;
-  //         loadRepos();
-  //         });    
-
-  //   var loadRepos = function () {
-  //       $http.get($scope.userData.repos_url)
-  //           .success(function (data) {
-  //               $scope.repoData = data;
-  //           });
-  //   };
-  // $scope.predicate = '-updated_at';
-  // };
-
 }]);
